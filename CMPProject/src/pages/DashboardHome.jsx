02e5e3c669467b15e8ecf5e308509bc57884ac80
Sridhar P <139834163+sridharpandiarajan@users.noreply.github.com>
2025-07-21 20:40:07 +0530

    Added 👷🏻‍♂️

--- CMPProject/src/pages/DashboardHome.jsx
+++ CMPProject/src/pages/DashboardHome.jsx
@@ -8,7 +8,7 @@ const DashboardHome = () => {
   return (
     <div className="dashboard-home-wrapper">
     <div className="dashboard-container">
-      <h2 className="main-title">Collin ManPower Agency</h2>
+      <h2 className="main-title">🏗️Collin ManPower Agency👷🏻‍♂️</h2>
       <h1 className="dashboard-heading">📋 Worker Management System</h1>
         <div className="action-grid">
           <div className="action-card" onClick={() => navigate('/add')}>
