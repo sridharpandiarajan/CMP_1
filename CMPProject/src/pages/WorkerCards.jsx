20d09b0e34d3e1578ae10da4f807d69ebe706d93
Sridhar P <139834163+sridharpandiarajan@users.noreply.github.com>
2025-07-21 20:44:54 +0530

    Update WorkerCards.jsx

--- CMPProject/src/pages/WorkerCards.jsx
+++ CMPProject/src/pages/WorkerCards.jsx
@@ -18,7 +18,7 @@ const WorkerCards = () => {
   };
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/workers')
+    fetch('https://cmp-1-uya2.onrender.com/routes/workers')
       .then(async (res) => {
         const contentType = res.headers.get("content-type");
         if (!res.ok) {
@@ -39,7 +39,7 @@ const WorkerCards = () => {
     if (!confirm) return;
 
     try {
-      const res = await fetch(`http://localhost:5000/api/workers/${id}`, {
+      const res = await fetch(`https://cmp-1-uya2.onrender.com/routes/workers/${id}`, {
         method: 'DELETE',
       });
 
